f4f2012211f83770517fa840259ed278fcd4c897
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2019-03-20 01:06:23 -0400

    algorithm.py updated with imports from data_cleaning

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,8 @@
-import pandas as pd, json
-from datetime import datetime, timedelta
-
-def datetime_str_to_object(fitbit_str):
-    """Helper function to convert fitbit datetime str into python datetime object"""
-    return datetime.strptime(fitbit_str, "%Y-%m-%dT%H:%M:%S.000")
+from data_cleaning import get_dataframe
 
 
 def main():
-	with open('data/alta_hr/2019.03.19.heart.json') as f:
-		data = json.load(f)
-
-	print(data)
+	print(get_dataframe('2019-03-19'))
 
 
 if __name__ == '__main__':
