0205c71e94f09ea6ed2700b401068ac1f2a529b0
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2019-03-20 00:51:10 -0400

    data cleaning v2

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,7 +5,7 @@ def datetime_str_to_object(fitbit_str):
     return datetime.datetime.strptime(fitbit_str, "%Y-%m-%dT%H:%M:%S.000")
 
 def get_heart_rate_data(date):
-    """Returns the dictionary of {time, heart_rate} for the given date(string)"""
+    """Returns the dictionary of {time, heart_rate} for the given date"""
     times, heart_rates = [], []
     # get heart data from the data file
     with open('data/alta_hr/'+date+'-heart.json') as file:
@@ -17,7 +17,7 @@ def get_heart_rate_data(date):
     return {'time':times, 'heart_rate':heart_rates}
 
 def get_dataframe(date):
-    """Returns the df of time(index), heart_rate, sleep_stage given the date(string)"""
+    """Returns the df of time(index), heart_rate, sleep_stage, half_mins_passed"""
     # create pandas dataframe from json, resample 30 seconds and write mean(integer) of heart rates
     df = pd.DataFrame(get_heart_rate_data(date)).set_index('time').resample('30s').mean().fillna(0).astype(int)
     df['sleep_stage'] = np.nan # fill sleep_stages with nan values
@@ -29,5 +29,8 @@ def get_dataframe(date):
         end_time = datetime_str_to_object(data['sleep'][0]['endTime']) # sleep end time
         for item in data['sleep'][0]['levels']['data']:
             df.loc[datetime_str_to_object(item['dateTime']), 'sleep_stage'] = item['level']
-
-    return df.loc[start_time:end_time,].fillna(method='ffill')
+            
+    df = df.loc[start_time:end_time,].fillna(method='ffill')
+    df['half_mins_passed'] = np.arange(len(df))
+    
+    return df[['half_mins_passed','heart_rate','sleep_stage']] # correct order
