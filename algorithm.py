d89aaa93a1cd28ffd62ce83cc1f57bd7ba231c7d
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2019-03-26 22:48:23 -0400

    accuracy down to 60

--- algorithm.py
+++ algorithm.py
@@ -5,18 +5,18 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 def main():
-	update_data_files()
-	# df = import_dataframes()
+	# update_data_files()
+	df = import_dataframes()
 
-	# X = df[["half_mins_passed","heart_rate"]]
-	# y = df['sleep_stage']
+	X = df[["half_mins_passed","heart_rate"]]
+	y = df['sleep_stage']
 
-	# model = SVC(kernel='rbf', C=1E10, gamma='auto')
-	# X1, X2, y1, y2 = train_test_split(X, y, random_state=0, train_size=0.5, test_size=0.5)
-	# model.fit(X1, y1)
+	model = SVC(kernel='rbf', C=1E10, gamma='auto')
+	X1, X2, y1, y2 = train_test_split(X, y, random_state=0, train_size=0.5, test_size=0.5)
+	model.fit(X1, y1)
 
-	# y2_model = model.predict(X2)
-	# print(accuracy_score(y2, y2_model))
+	y2_model = model.predict(X2)
+	print(accuracy_score(y2, y2_model))
 
 
 if __name__ == '__main__':
